9d36e19c538f51607ab78a6823ba78c3d88cfbe2
o06375718 <mervenur123ozan@gmail.com>
2020-11-08 15:34:49 +0000

    lab Qs

---
+++ main.py
@@ -0,0 +1,10 @@
+a= int(input("Enter  number"))
+b= int(input("Enter number"))
+c= int(input("Enter number"))
+if b<a :
+    print(b)
+elif c<b:
+    print(c)
+elif a<c :
+    print(a)
+print("Minimum number:",min(a,b,c))
