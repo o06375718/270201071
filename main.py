1b6ef5807f4e3b048c9426c6bd9c310fe4af1b59
o06375718 <mervenur123ozan@gmail.com>
2020-11-09 08:57:43 +0000

    leap.years

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-number= int(input("Enter a number:"))
-if number<10 :
-   totals =number
+year= int(input("Enter year:"))
+if year%4 == 0:
+ print("Leap year")
 else :
-  ones= number %10 
-  tens=( number//10)%10
-  totals = ones +tens
-  print("The sum of last two digit",totals)
+  print("Not leap year")
