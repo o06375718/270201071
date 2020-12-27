9e312b3fa27b04edcee71adeeeba137b4071d563
o06375718 <mervenur123ozan@gmail.com>
2020-12-27 13:28:27 +0000

    exercise

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-year= int(input("Enter year:"))
-if year%4 == 0:
- print("Leap year")
-else :
-  print("Not leap year")
+travelling=int("Yes/No")
+while travelling=="Yes":
+   num=int(input("number of people travelling"))
+   for travel in range(1,num+1):
+      name=input("Please enter a name")
+
+      age=input("Please enter age:")
+
+      sex=input("Male/Female")
+
+      print(name)
+
+      print(age)
+
+      print(sex)
+      
+travelling=input("Ooops Forgot someone")    
