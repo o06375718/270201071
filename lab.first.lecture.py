3c7fa47b9640dfc1d6c8f91cf6b6bd890c6efa13
o06375718 <mervenur123ozan@gmail.com>
2021-01-20 21:37:53 +0000

    lab.exercises

---
+++ lab.first.lecture.py
@@ -0,0 +1,44 @@
+######## Animals ##########
+animals_number=10
+chickens=6
+rooters=animals_number-chickens
+print(rooters)
+
+
+######### Math Expression #########
+
+x=1
+y=4 
+z=0.25
+result=(((2*x+y)**2)*z**0.5)/(x)**0.5+(y)**0.5
+print(result)
+
+######### Solving Equations ##########
+a=2
+b=6
+c=-20
+x=(-b+(b*b-4*a*c)**0.5)/(2*a)
+print(x)
+
+
+###### Temperature #######
+
+T=float(input("Enter Celcius Temparature:"))
+F = T *1.8+32
+print(F)
+
+
+######## Hypotenuse #########
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
+c=(a**2+b**2)**0.5
+print(c)
+
+
+######### Velocity #########
+#3.6 km/h = 1 m/s
+V1=80
+V2=70
+distance=490
+hour=340/150
+print(hour)
